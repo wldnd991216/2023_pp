410493f3959ef4bef8bc2d6a4c87d62718f7d6e6
201827061 <wldnd0567@naver.com>
2023-03-20 11:52:49 +0900

    2023 03 20

---
+++ Lecture03/README.md
@@ -0,0 +1,5 @@
+Lecture 03 -VI 에디터 실습및 C++ 기초-
+*VI 에디터를 실습한다.
+*C++ 기초를 배운다
+vim LEADME.md 로 만듦
+
---
+++ Lecture03/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	//오 주석도 되네
+	std::cout<<"ㅇ,ㅡㅇ어어\n 얶 엑"<<std::endl; //n은 줄바꿈 endl 줄 끝
+						     //std 안하면 에러뜸 표준안지켜서
+						     
+	return 0;
+				
+	
+}
+
